1de30a229976754645bf95914e8a889abccf7bec
Neevs1 <neevan.redkar@gmail.com>
2025-07-05 17:55:08 +0530

    Added keypress

--- Drum Kit/Drum Kit Starting Files/index.js
+++ Drum Kit/Drum Kit Starting Files/index.js
@@ -1,32 +1,47 @@
 for(var i=0;i<document.querySelectorAll(".drum").length;i++){
     document.querySelectorAll(".drum")[i].addEventListener("click", function() {
         var button= this.innerHTML;
-        switch (button) {
-            case 'w':
+        makesound(button);
+        
+
+    });
+
+}
+
+
+
+document.addEventListener("keydown", function(event) {
+  makesound(event.key);   
+}  
+)
+
+function makesound(key){
+    switch (key) {
+            case "w":
                 var tom1 = new Audio('sounds/tom-1.mp3');
                 tom1.play();
                 break;
-            case 'a':
+            case "a":
                 var tom2 = new Audio('sounds/tom-2.mp3');
                 tom2.play();
             break;
-            case 's':
+            case "s":
                 var tom3 = new Audio('sounds/tom-3.mp3');
                 tom3.play()
                 break;
-            case 'd':
+            case "d":
                 var tom4 = new Audio('sounds/tom-4.mp3');
                 tom4.play();
                 break;
-            case 'j':
+            case "j":
                 var snare = new Audio('sounds/snare.mp3');
                 snare.play();
                 break;
-            case 'k':
+            case "k":
                 var crash = new Audio('sounds/crash.mp3');
                 crash.play();
                 break;
-            case 'l':
+            case "l":
                 var kick = new Audio('sounds/kick-bass.mp3');
                 kick.play();
                 break;
@@ -36,10 +51,5 @@ for(var i=0;i<document.querySelectorAll(".drum").length;i++){
             default:
                 break;
         }
-
-    });
-
 }
-
-
 
